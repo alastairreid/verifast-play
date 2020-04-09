9ee315c74b790cd68f39a11727149cc80d40161a
Alastair Reid <adreid@google.com>
2020-04-09 10:11:27 +0100

    wip

--- src/dlist.h
+++ src/dlist.h
@@ -37,17 +37,53 @@ predicate dlist2(struct node *n1, struct node *p1, struct node *n2, struct node 
       &*& dlist2(n1, p1, p2, p3)
 ;
 
-lemma void join_dlist(
+lemma void singleton1(struct node *p, struct node *i, struct node *n)
+	requires malloc_block_node(i) &*& i->data |-> _ &*& i->next |-> n &*& i->prev |-> p &*& i != n &*& i != p;
+	ensures dlist(i,p,n,i);
+{
+	close dlist(n,i,n,i);
+	close dlist(i,p,n,i);
+}
+
+lemma void join_dlist1(
 	struct node *n1, struct node *p1,
 	struct node *n2, struct node *p2,
-	struct node *n3, struct node *p3);
+	struct node *n3, struct node *p3)
 	requires dlist(n1,p1,n2,p2) &*& dlist(n2,p2,n3,p3);
 	ensures dlist(n1,p1,n3,p3);
+{
+	if (n1 == n2 && p1 == p2) {
+		open dlist(n1,p1,n2,p2);
+	} else if (n2 == n3 && p2 == p3) {
+		open dlist(n2,p2,n3,p3);
+	} else if (n1 == n3 && p1 == p3) {
+		open dlist(n1,p1,n2,p2);
+		join_dlist1(n1->next,n1,n2,p2,n3,p3);
+		close dlist(n1,p1,n3,p3);
+	} else {
+		open dlist(n1,p1,n2,p2);
+		join_dlist1(n1->next,n1,n2,p2,n3,p3);
+		close dlist(n1,p1,n3,p3);
+	}
+}
 
-// changing view of a doubly linked circular list
-lemma void dlist_to_dlist2(struct node *p, struct node *i, struct node *n);
-	requires dlist(i,n,p,i);
-	ensures  dlist2(i,n,p,i);
+#if 0
+// changing view of a non-empty doubly linked circular list
+lemma void dlist_to_dlist2(struct node *i)
+	requires malloc_block_node(i) &*& i->data |-> _ &*& i->next |-> ?n &*& i->prev |-> ?p &*& dlist(n,i,i,p);
+	ensures  malloc_block_node(i) &*& i->data |-> _ &*& i->next |-> n &*& i->prev |-> p &*& dlist2(n,i,i,p);
+{
+	open dlist(n,i,i,p);
+	if (i == n && i == p) {
+	} else {
+		close dlist(n,i,n,i);
+		close dlist(i,p,n,i);
+		join_dlist(n->next,n,i,p,n,i);
+		dlist_to_dlist2(n);
+	}
+	close dlist2(n,i,i,p);
+}
+#endif
 
 // Circular, doubly-linked list
 predicate clist(struct node *i) =
