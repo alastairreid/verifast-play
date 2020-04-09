5e95d68d9229a6906a30b4ec1b25e67779bb8822
Alastair Reid <adreid@google.com>
2020-04-09 03:59:29 +0100

    add dlist2

--- src/dlist.h
+++ src/dlist.h
@@ -25,6 +25,18 @@ predicate dlist(struct node *n1, struct node *p1, struct node *n2, struct node *
       &*& dlist(n3, n1, n2, p2)
 ;
 
+// Alternate predicate for doubly linked lists that recurses
+// along "prev" links
+predicate dlist2(struct node *n1, struct node *p1, struct node *n2, struct node *p2) =
+    (n1 == n2 && p1 == p2)
+    ? true
+    : malloc_block_node(p2)
+      &*& p2->data |-> _
+      &*& p2->next |-> n2
+      &*& p2->prev |-> ?p3
+      &*& dlist2(n1, p1, p2, p3)
+;
+
 lemma void join_dlist(
 	struct node *n1, struct node *p1,
 	struct node *n2, struct node *p2,
@@ -32,13 +44,10 @@ lemma void join_dlist(
 	requires dlist(n1,p1,n2,p2) &*& dlist(n2,p2,n3,p3);
 	ensures dlist(n1,p1,n3,p3);
 
-// This can't hold for arbitrary n2/p2 - what is an appropriate restriction?
-lemma void split_dlist(
-	struct node *n1, struct node *p1,
-	struct node *n2, struct node *p2,
-	struct node *n3, struct node *p3);
-	requires dlist(n1,p1,n3,p3);
-	ensures dlist(n1,p1,n2,p2) &*& dlist(n2,p2,n3,p3);
+// changing view of a doubly linked circular list
+lemma void dlist_to_dlist2(struct node *p, struct node *i, struct node *n);
+	requires dlist(i,n,p,i);
+	ensures  dlist2(i,n,p,i);
 
 // Circular, doubly-linked list
 predicate clist(struct node *i) =
@@ -48,7 +57,8 @@ predicate clist(struct node *i) =
       &*& i->data |-> _
       &*& i->next |-> ?n
       &*& i->prev |-> ?p
-      &*& dlist(n, i, i, p);
+      &*& dlist(n, i, i, p)
+;
 @*/
 
 struct node *insert_after(int x, struct node *xs);
