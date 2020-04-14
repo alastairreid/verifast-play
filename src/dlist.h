aa9f3680167fed85d781218484579af9e8f53c2e
Alastair Reid <adreid@google.com>
2020-04-14 11:44:30 +0100

    Some random changes

--- src/dlist.h
+++ src/dlist.h
@@ -12,17 +12,19 @@ struct node {
 };
 
 /*@
-// Doubly linked list segment with two sets of links:
+// Doubly linked acyclic list segment with two sets of links:
 // - Forward list from n1 to n2 through the "next" fields
 // - Backward list from p2 to p1 through the "prev" fields
-predicate dlist(struct node *n1, struct node *p1, struct node *n2, struct node *p2) =
+// (To turn this into a cyclic list, the ends are joined
+// together by inserting a "root" node.)
+predicate dlist1(struct node *n1, struct node *p1, struct node *n2, struct node *p2) =
     (n1 == n2 && p1 == p2)
     ? true
     : malloc_block_node(n1)
       &*& n1->data |-> _
       &*& n1->next |-> ?n3
       &*& n1->prev |-> p1
-      &*& dlist(n3, n1, n2, p2)
+      &*& dlist1(n3, n1, n2, p2)
 ;
 
 // Alternate predicate for doubly linked lists that recurses
@@ -39,36 +41,44 @@ predicate dlist2(struct node *n1, struct node *p1, struct node *n2, struct node 
 
 lemma void singleton1(struct node *p, struct node *i, struct node *n)
 	requires malloc_block_node(i) &*& i->data |-> _ &*& i->next |-> n &*& i->prev |-> p &*& i != n &*& i != p;
-	ensures dlist(i,p,n,i);
+	ensures dlist1(i,p,n,i);
 {
-	close dlist(n,i,n,i);
-	close dlist(i,p,n,i);
+	close dlist1(n,i,n,i);
+	close dlist1(i,p,n,i);
 }
 
-lemma void join_dlist1(
-	struct node *n1, struct node *p1,
-	struct node *n2, struct node *p2,
-	struct node *n3, struct node *p3)
-	requires dlist(n1,p1,n2,p2) &*& dlist(n2,p2,n3,p3);
-	ensures dlist(n1,p1,n3,p3);
+lemma void snoc(
+	struct node *n1,
+	struct node *p1,
+	struct node *n2,
+	struct node *p2,
+	struct node *n3)
+	requires dlist1(n1,p1,n2,p2) &*& malloc_block_node(n2) &*& n2->data |-> _ &*& n2->next |-> n3 &*& n2->prev |-> p2 &*& n1 != n3 &*& p1 != n2;
+	ensures dlist1(n1,p1,n3,n2);
 {
 	if (n1 == n2 && p1 == p2) {
-		open dlist(n1,p1,n2,p2);
-	} else if (n2 == n3 && p2 == p3) {
-		open dlist(n2,p2,n3,p3);
-	} else if (n1 == n3 && p1 == p3) {
-		open dlist(n1,p1,n2,p2);
-		join_dlist1(n1->next,n1,n2,p2,n3,p3);
-		close dlist(n1,p1,n3,p3);
+		open dlist1(n1,p1,n2,p2);
+		singleton1(p1,n2,n3);
 	} else {
-		open dlist(n1,p1,n2,p2);
-		join_dlist1(n1->next,n1,n2,p2,n3,p3);
-		close dlist(n1,p1,n3,p3);
+		open dlist1(n1,p1,n2,p2);
+		snoc(n1->next,n1,n2,p2,n3);
 	}
 }
 
 #if 0
-// changing view of a non-empty doubly linked circular list
+lemma void dlist_to_dlist2(
+	struct node *n1,
+	struct node *p1,
+	struct node *n2,
+	struct node *p2)
+	requires dlist1(n1,p1,n2,p2);
+	ensures  dlist2(n1,p1,n2,p2);
+{
+}
+#endif
+
+#if 0
+// changing view of a doubly-linked acyclic list
 lemma void dlist_to_dlist2(struct node *i)
 	requires malloc_block_node(i) &*& i->data |-> _ &*& i->next |-> ?n &*& i->prev |-> ?p &*& dlist(n,i,i,p);
 	ensures  malloc_block_node(i) &*& i->data |-> _ &*& i->next |-> n &*& i->prev |-> p &*& dlist2(n,i,i,p);
@@ -93,7 +103,7 @@ predicate clist(struct node *i) =
       &*& i->data |-> _
       &*& i->next |-> ?n
       &*& i->prev |-> ?p
-      &*& dlist(n, i, i, p)
+      &*& dlist1(n, i, i, p)
 ;
 @*/
 
